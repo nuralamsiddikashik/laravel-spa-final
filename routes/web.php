dfe899a9d19dc006cac9e0e69ec746aaf54d3d57
nuralamsiddikashik <nuralamsiddikashik@gmail.com>
2021-08-07 19:54:28 +0600

    UPDATE CODE

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,19 @@ use Illuminate\Support\Facades\Route;
 Route::get( '/', function () {
     return view( 'welcome' );
 } );
-Route::any('{any}', function () {
-    return view('welcome');
-})->where('any', '.*');
-
+Route::any( '{any}', function () {
+    return view( 'welcome' );
+} )->where( ['any' => '^((?!app).)*$'] );
 
 Route::get( '/app', [AdminController::class, 'index'] )->name( 'admin' );
-Route::any( '{any}', [AdminController::class, 'index'] )->where( 'any', '.*' );
+// Route::any( '{any}', [AdminController::class, 'index'] )->where( 'any', '.*' );
+
+// Route::get( '/', function () {
+//     return view( 'welcome' );
+// } );
+// Route::any('{any}', function () {
+//     return view('welcome');
+// })->where('any', '.*');
+
+// Route::get( '/app', [AdminController::class, 'index'] )->name( 'admin' );
+// Route::any( '{any}', [AdminController::class, 'index'] )->where( 'any', '.*' );
